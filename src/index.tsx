dfeb00c4b6c8372573179bfab16197dad58364bd
IgorAvshtol <iharrwe@gmail.com>
2021-07-28 00:43:14 +0300

    mass update1

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import AppWithRedux from "./AppWithRedux";
+import App from "./App";
 import {Provider} from "react-redux";
 import {store} from "./state/store";
 
 ReactDOM.render(<Provider store={store}>
-        <AppWithRedux/>
+        <App/>
     </Provider>
     , document.getElementById('root'));
 
